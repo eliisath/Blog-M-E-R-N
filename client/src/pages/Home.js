b0b91fb5753b5e9c7b23af2a2d1015802b49c76a
Elisa <elisath01@gmail.com>
2024-03-07 22:40:38 -0300

    Construyendo Blog con MongoDB, Express, React y Node.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mb-20'>
+      <h1 className='sm:text-4xl text-2xl text-center font-bold my-6 text-gray-900-'>
+        Bienvenido a Elisa Tech
+      </h1>
+      <h5 className='sm:text-2xl text-2xl text-center font-bold my-6 text-gray-900-'>
+        El Mejor Blog de Tecnologia
+      </h5>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        loremIn ut nostrud elit ut reprehenderit commodo. Adipisicing officia proident adipisicing id id veniam cupidatat sunt.
+        Enim incididunt ullamco minim ea eiusmod sint ullamco est. Proident pariatur quis laborum sunt voluptate velit mollit ea laborum anim nulla nostrud.
+        Elit esse aliquip non dolore ex. In qui ullamco commodo id minim labore culpa pariatur irure. Eiusmod anim magna magna reprehenderit ad aliquip velit.
+        Reprehenderit anim minim reprehenderit officia qui fugiat esse quis velit excepteur veniam. Minim id non et incididunt labore duis id fugiat sunt incididunt magna.
+        Sunt reprehenderit commodo cillum dolore aute sunt aute velit. Officia irure labore proident ex adipisicing id esse sit Lorem eiusmod nostrud nisi consequat.
+      </p>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        loremIn ut nostrud elit ut reprehenderit commodo. Adipisicing officia proident adipisicing id id veniam cupidatat sunt.
+        Enim incididunt ullamco minim ea eiusmod sint ullamco est. Proident pariatur quis laborum sunt voluptate velit mollit ea laborum anim nulla nostrud.
+        Elit esse aliquip non dolore ex. In qui ullamco commodo id minim labore culpa pariatur irure. Eiusmod anim magna magna reprehenderit ad aliquip velit.
+        Reprehenderit anim minim reprehenderit officia qui fugiat esse quis velit excepteur veniam. Minim id non et incididunt labore duis id fugiat sunt incididunt magna.
+        Sunt reprehenderit commodo cillum dolore aute sunt aute velit. Officia irure labore proident ex adipisicing id esse sit Lorem eiusmod nostrud nisi consequat.
+        Nisi duis officia cupidatat sit magna mollit in ex dolor labore dolore dolor.
+      </p>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        loremIn ut nostrud elit ut reprehenderit commodo. Adipisicing officia proident adipisicing id id veniam cupidatat sunt.
+        Enim incididunt ullamco minim ea eiusmod sint ullamco est. Proident pariatur quis laborum sunt voluptate velit mollit ea laborum anim nulla nostrud.
+        Elit esse aliquip non dolore ex. In qui ullamco commodo id minim labore culpa pariatur irure. Eiusmod anim magna magna reprehenderit ad aliquip velit.
+        Reprehenderit anim minim reprehenderit officia qui fugiat esse quis velit excepteur veniam. Minim id non et incididunt labore duis id fugiat sunt incididunt magna.
+        Sunt reprehenderit commodo cillum dolore aute sunt aute velit. Officia irure labore proident ex adipisicing id esse sit Lorem eiusmod nostrud nisi consequat.
+        Nisi duis officia cupidatat sit magna mollit in ex dolor labore dolore dolor.
+      </p>
+    </div>
+  )
+}
+
+export default Home
