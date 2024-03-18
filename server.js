df1af4cf93baadd64611ddf2b1c0b00948a6eab4
Elisa <elisath01@gmail.com>
2024-03-18 17:55:41 -0300

    Conectando a base de datos con Mongo

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const { MongoClient } = require("mongodb");
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json({ extended: false }));
+
+const withDB = async (operations, res) => {
+    try {
+        const client = await MongoClient.connect("mongodb://localhost:27017/");
+        const db = client.db("mernblog");
+        await operations(db);
+        client.close();
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error connecting to database", error: error });
+    }
+};
+
+app.get("/api/articles/:name", async (req, res) => {
+    withDB(async (db) => {
+        const articleName = req.params.name;
+        const articleInfo = await db
+            .collection("articles")
+            .findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+    }, res)
+});
+
+app.post("/api/articles/:name/add-comments", (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    withDB(async (db) => {
+        const articleInfo = await db
+            .collection('articles')
+            .findOne({ name: articleName });
+        await db.collection("articles").updateOne(
+            { name: articleName },
+            {
+                $set: {
+                    comments: articleInfo.comments.concat({ username, text }),
+                },
+            }
+        );
+        const updateArticleInfo = await db.collection("articles").findOne({ name: articleName })
+        res.status(200).json(updateArticleInfo);
+    }, res);
+});
+
+app.listen(8000, () => console.log(`Server started at port ${PORT}`));
